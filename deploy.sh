fd04fa8c261466529ba50447db8efb6609a4f172
Nam Pham <nampham@Nams-MacBook-Pro.local>
2025-03-11 22:38:06 +0700

    feat: add deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# build
+docker build -t y0urs3lf/multi-client:latest -t y0urs3lf/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t y0urs3lf/multi-server:latest -t y0urs3lf/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t y0urs3lf/multi-worker:latest -t y0urs3lf/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+# push latest to docker hub
+docker push y0urs3lf/multi-client:latest
+docker push y0urs3lf/multi-server:latest
+docker push y0urs3lf/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push y0urs3lf/multi-client:$SHA
+docker push y0urs3lf/multi-server:$SHA
+docker push y0urs3lf/multi-worker:$SHA
 
+# update running deployments
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=y0urs3lf/multi-server:$SHA
+kubectl set image deployments/client-deployment client=y0urs3lf/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=y0urs3lf/multi-worker:$SHA
